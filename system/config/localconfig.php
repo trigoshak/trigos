f0332faf753e40f022d225dd75b5f431e9808291
luccapeer <luccapeer@github.com>
2018-11-01 17:57:45 +0100

    push all files

---
+++ system/config/localconfig.php
@@ -0,0 +1,21 @@
+<?php
+
+### INSTALL SCRIPT START ###
+$GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
+$GLOBALS['TL_CONFIG']['installPassword'] = '$2y$10$LqwTG4BnGDLv3WUiuTO1MeBg9I0p.OokUoN9pc3QL5ZgUTmTB/WSm';
+$GLOBALS['TL_CONFIG']['encryptionKey'] = 'd8ae6a2a736e0fed5949a2f663e07f10';
+$GLOBALS['TL_CONFIG']['dbDriver'] = 'MySQLi';
+$GLOBALS['TL_CONFIG']['dbHost'] = 'localhost';
+$GLOBALS['TL_CONFIG']['dbUser'] = 'root';
+$GLOBALS['TL_CONFIG']['dbPass'] = '';
+$GLOBALS['TL_CONFIG']['dbDatabase'] = 'trigos';
+$GLOBALS['TL_CONFIG']['dbPconnect'] = false;
+$GLOBALS['TL_CONFIG']['dbCharset'] = 'UTF8';
+$GLOBALS['TL_CONFIG']['dbPort'] = 3306;
+$GLOBALS['TL_CONFIG']['dbSocket'] = '';
+$GLOBALS['TL_CONFIG']['adminEmail'] = 'bennibraun00@gmail.com';
+$GLOBALS['TL_CONFIG']['latestVersion'] = '3.5.36';
+$GLOBALS['TL_CONFIG']['maintenanceMode'] = false;
+$GLOBALS['TL_CONFIG']['imageWidth'] = 1920;
+$GLOBALS['TL_CONFIG']['imageHeight'] = 1080;
+### INSTALL SCRIPT STOP ###
