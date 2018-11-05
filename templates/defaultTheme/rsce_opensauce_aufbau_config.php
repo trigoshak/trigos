cb2b9d1cee35e81f41e92be5a5300b2c0aa2bb44
Lucca Peer <lucca.peer@outlook.com>
2018-11-05 10:25:32 +0100

    add all files

--- templates/defaultTheme/rsce_opensauce_aufbau_config.php
+++ templates/defaultTheme/rsce_opensauce_aufbau_config.php
@@ -7,22 +7,42 @@ return array(
 	),
 	'types' => array('content'),
 	'standardFields' => array('cssID'),
-	'fields' => array(
-		'boxes' => array(
-			'label' => array('Abteilungen', ''),
-			'elementLabel' => 'Abteilung %s',
-			'inputType' => 'list',
-			'fields' => array(
-				'headline' => array(
-					'label' => array('Abteilung', ''),
-					'inputType' => 'text',
-				),
-				'text' => array(
-					'label' => array('Anzahl Mitarbeiter', ''),
-					'inputType' => 'text',
-					'eval' => array('rte' => 'tinyMCE'),
-				),
+		'fields' => array(
+			'headline1' => array(
+				'label' => array('1. Abteilung', ''),
+				'inputType' => 'text',
+			),
+			'text1' => array(
+				'label' => array('Anzahl Mitarbeiter', ''),
+				'inputType' => 'text',
+				'eval' => array('rte' => 'tinyMCE'),
+			),
+			'headline2' => array(
+				'label' => array('2. Abteilung', ''),
+				'inputType' => 'text',
+			),
+			'text2' => array(
+				'label' => array('Anzahl Mitarbeiter', ''),
+				'inputType' => 'text',
+				'eval' => array('rte' => 'tinyMCE'),
+			),
+			'headline3' => array(
+				'label' => array('3. Abteilung', ''),
+				'inputType' => 'text',
+			),
+			'text3' => array(
+				'label' => array('Anzahl Mitarbeiter', ''),
+				'inputType' => 'text',
+				'eval' => array('rte' => 'tinyMCE'),
+			),
+			'headline4' => array(
+				'label' => array('4. Abteilung', ''),
+				'inputType' => 'text',
+			),
+			'text4' => array(
+				'label' => array('Anzahl Mitarbeiter', ''),
+				'inputType' => 'text',
+				'eval' => array('rte' => 'tinyMCE'),
 			),
 		),
-	),
 );
--- templates/defaultTheme/rsce_opensauce_jobs_config.php
+++ templates/defaultTheme/rsce_opensauce_jobs_config.php
@@ -6,23 +6,37 @@ return array(
 		'Erzeugt nebeneinander dargestellte Boxen mit Bild und Text',
 	),
 	'types' => array('content'),
-	'standardFields' => array('cssID', 'headline', 'columns'),
+	'standardFields' => array('cssID'),
 	'fields' => array(
-		'size' => array(
-			'label' => array('Bildbreite und Bildhöhe', ''),
-			'inputType' => 'imageSize',
-			'options' => \System::getImageSizes(),
-			'reference' => &$GLOBALS['TL_LANG']['MSC'],
-			'eval' => array(
-				'rgxp' => 'digit',
-				'includeBlankOption' => true,
-			),
+		'headline' => array(
+			'label' => array('Überschrift', ''),
+			'inputType' => 'text',
+			'eval' => array('rte' => 'tinyMCE'),
+		),
+		'text' => array(
+			'label' => array('Text', 'Absatztext'),
+			'inputType' => 'textarea',
+			'eval' => array('tl_class' => 'w50'),
+		),
+		'url' => array(
+			'label' => array('URL', 'Beispiele: {{link_url::seitenalias}} (Alias, ID oder anderer Inserttag), http://example.com'),
+			'inputType' => 'url',
+			'eval' => array('tl_class' => 'w50'),
+		),
+		'newWindow' => array(
+			'label' => $GLOBALS['TL_LANG']['MSC']['target'],
+			'inputType' => 'checkbox',
+			'eval' => array('tl_class' => 'w50 m12'),
 		),
 		'boxes' => array(
-			'label' => array('Boxen', ''),
+			'label' => array('Box', ''),
 			'elementLabel' => 'Box %s',
 			'inputType' => 'list',
 			'fields' => array(
+				'headline' => array(
+					'label' => array('Überschrift', ''),
+					'inputType' => 'text',
+				),
 				'image' => array(
 					'label' => array('Bild', ''),
 					'inputType' => 'fileTree',
@@ -32,24 +46,10 @@ return array(
 						'extensions' => \Config::get('validImageTypes'),
 					),
 				),
-				'headline' => array(
-					'label' => array('Überschrift', ''),
+				'subheadline' => array(
+					'label' => array('Kategorie Überschrift', ''),
 					'inputType' => 'text',
-				),
-				'text' => array(
-					'label' => array('Beschreibung', ''),
-					'inputType' => 'textarea',
 					'eval' => array('rte' => 'tinyMCE'),
-				),
-				'url' => array(
-					'label' => array('URL', 'Beispiele: {{link_url::seitenalias}} (Alias, ID oder anderer Inserttag), http://example.com'),
-					'inputType' => 'url',
-					'eval' => array('tl_class' => 'w50'),
-				),
-				'newWindow' => array(
-					'label' => $GLOBALS['TL_LANG']['MSC']['target'],
-					'inputType' => 'checkbox',
-					'eval' => array('tl_class' => 'w50 m12'),
 				),
 			),
 		),
