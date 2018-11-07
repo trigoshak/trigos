2b577bf202c67104b436993ea135543bbb6daa99
Lucca Peer <lucca.peer@outlook.com>
2018-11-07 10:54:43 +0100

    push all files

--- templates/defaultTheme/rsce_opensauce_ueberUns_config.php
+++ templates/defaultTheme/rsce_opensauce_ueberUns_config.php
@@ -2,56 +2,80 @@
 
 return array(
 	'label' => array(
-		'Ueber Uns',
+		'Ueber uns',
 		'Erzeugt nebeneinander dargestellte Boxen mit Bild und Text',
 	),
 	'types' => array('content'),
-	'standardFields' => array('cssID', 'headline', 'columns'),
+	'standardFields' => array('cssID'),
 	'fields' => array(
-		'size' => array(
-			'label' => array('Bildbreite und Bildhöhe', ''),
-			'inputType' => 'imageSize',
-			'options' => \System::getImageSizes(),
-			'reference' => &$GLOBALS['TL_LANG']['MSC'],
+		'image1' => array(
+			'label' => array('Bild', ''),
+			'inputType' => 'fileTree',
 			'eval' => array(
-				'rgxp' => 'digit',
-				'includeBlankOption' => true,
+				'fieldType' => 'radio',
+				'filesOnly' => true,
+				'extensions' => \Config::get('validImageTypes'),
 			),
 		),
-		'boxes' => array(
-			'label' => array('Boxen', ''),
-			'elementLabel' => 'Box %s',
-			'inputType' => 'list',
-			'fields' => array(
-				'image' => array(
-					'label' => array('Bild', ''),
-					'inputType' => 'fileTree',
-					'eval' => array(
-						'fieldType' => 'radio',
-						'filesOnly' => true,
-						'extensions' => \Config::get('validImageTypes'),
-					),
-				),
-				'headline' => array(
-					'label' => array('Überschrift', ''),
-					'inputType' => 'text',
-				),
-				'text' => array(
-					'label' => array('Beschreibung', ''),
-					'inputType' => 'textarea',
-					'eval' => array('rte' => 'tinyMCE'),
-				),
-				'url' => array(
-					'label' => array('URL', 'Beispiele: {{link_url::seitenalias}} (Alias, ID oder anderer Inserttag), http://example.com'),
-					'inputType' => 'url',
-					'eval' => array('tl_class' => 'w50'),
-				),
-				'newWindow' => array(
-					'label' => $GLOBALS['TL_LANG']['MSC']['target'],
-					'inputType' => 'checkbox',
-					'eval' => array('tl_class' => 'w50 m12'),
-				),
+		'kategorie1' => array(
+			'label' => array('Kategorie 1', ''),
+			'inputType' => 'text',
+		),
+		'kategorie1_text1' => array(
+			'label' => array('Paragraph 1', ''),
+			'inputType' => 'textarea',
+			'eval' => array('rte' => 'tinyMCE'),
+		),
+		'kategorie1_text2' => array(
+			'label' => array('Paragraph 2', ''),
+			'inputType' => 'textarea',
+			'eval' => array('rte' => 'tinyMCE'),
+		),
+		'image2' => array(
+			'label' => array('Bild', ''),
+			'inputType' => 'fileTree',
+			'eval' => array(
+				'fieldType' => 'radio',
+				'filesOnly' => true,
+				'extensions' => \Config::get('validImageTypes'),
 			),
+		),
+		'kategorie2' => array(
+			'label' => array('Kategorie 2', ''),
+			'inputType' => 'text',
+		),
+		'kategorie2_text1' => array(
+			'label' => array('Paragraph 1', ''),
+			'inputType' => 'textarea',
+			'eval' => array('rte' => 'tinyMCE'),
+		),
+		'kategorie2_text2' => array(
+			'label' => array('Paragraph 2', ''),
+			'inputType' => 'textarea',
+			'eval' => array('rte' => 'tinyMCE'),
+		),
+		'image3' => array(
+			'label' => array('Bild', ''),
+			'inputType' => 'fileTree',
+			'eval' => array(
+				'fieldType' => 'radio',
+				'filesOnly' => true,
+				'extensions' => \Config::get('validImageTypes'),
+			),
+		),
+		'kategorie3' => array(
+			'label' => array('Kategorie 3', ''),
+			'inputType' => 'text',
+		),
+		'kategorie3_text1' => array(
+			'label' => array('Paragraph 1', ''),
+			'inputType' => 'textarea',
+			'eval' => array('rte' => 'tinyMCE'),
+		),
+		'kategorie3_text2' => array(
+			'label' => array('Paragraph 2', ''),
+			'inputType' => 'textarea',
+			'eval' => array('rte' => 'tinyMCE'),
 		),
 	),
 );
